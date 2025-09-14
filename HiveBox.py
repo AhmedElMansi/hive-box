43e80e76f8b141d8e3bf48f84f523cc8ade6bdda
Ahmed Elmansi <ahmed.manci@hotmail.com>
2025-09-15 00:34:15 +0300

    Fix app code

--- HiveBox.py
+++ HiveBox.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 app_version = "v0.0.2"
 
+
 @app.route("/version")
 def version():
     return {"version": app_version}
 
+
 @app.route("/temperature")
 def temperature():
     return {"temperature": 27.5}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
